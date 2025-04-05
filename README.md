9d2b62ee3751030d059d3ccafb93e6bd39ae6bda
Ingrid Munera <ingridmunera24@gmail.com>
2025-04-05 16:57:30 -0500

    eslint updated

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Trello Web Test Automation
 
 ## Description
+
 This project automates https://trello.com/en website using WebDriverIO and Page Object Model desing pattern.
 
 ## Setup
+
 1. Clone or download the repository.
 2. Run `npm install` to install dependencies.
 3. Run `npm test` to execute the tests.
 
 ## Execute an expecific test scenario with tag
+
 Run `npm run test -- --cucumberOpts.tagExpression='@tagName'`
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,19 +3,23 @@ import globals from "globals";
 import js from "@eslint/js";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"] },
   {
     files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        browser: "readonly",
+        $: "readonly",
+        $$: "readonly",
+      },
+    },
     plugins: { js },
-    extends: ["js/recommended"],
+    rules: {
+      "no-unused-vars": "warn",
+      quotes: ["warn", "single"],
+      semi: ["warn", "always"],
+    },
+    extends: ["plugin:@eslint/js/recommended"],
   },
 ]);
-
-export const scripts = {
-  lint: "eslint . --ext .js,.mjs,.cjs",
-  format: "prettier --write .",
-};
